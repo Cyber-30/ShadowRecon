997525394674974715b17a749ab0bdea6aff2b4c
SOURYA DUTTA <110114428+Cyber-30@users.noreply.github.com>
2025-07-03 20:41:14 +0530

    Update shadowrecon.py
    Update shadowrecon.py file
    
    add the whois info module

--- shadowrecon.py
+++ shadowrecon.py
@@ -1,13 +1,23 @@
 import click
+from rich.console import Console
 from modules.crtsh import fetch_crtsh_data
+from modules.whois import fetch_whois_info
+
+console = Console()
 
 @click.command()
 @click.argument('target')
 @click.option('--crt', is_flag=True, help='Gather SSL certificate data from crt.sh')
-def cli(target,crt):
+@click.option('--whois', is_flag=True, help='Gather WHOIS information')
+def cli(target, crt, whois):
     if crt:
-        click.echo(f"[*] Fetching info from crt.sh: {target}")
+        console.print(f"[bold green][*] Fetching info from crt.sh: {target}[/bold green]")
         fetch_crtsh_data(target)
+    elif whois:
+        console.print(f"[bold green][*] Fetching WHOIS info for {target}[/bold green]")
+        fetch_whois_info(target)
+    else:
+        console.print("[red][!] No option specified. Use --crt or --whois.[/red]")
 
 if __name__ == "__main__":
     cli()
